3db2eafc7929ae2902ba8b6a910c3e159226bd8f
Sameer Srivastava <sameer.srivastava@nearform.com>
2022-02-01 14:32:37 +0530

    chore: tests, reademe updated

--- index.js
+++ index.js
@@ -1,16 +1,5 @@
-const fastify = require('fastify')({ logger: true })
-
-fastify.get('/', async (request, reply) => {
-  return { hello: 'world' }
-})
-
-const start = async () => {
-  try {
-    await fastify.listen(3000)
-  } catch (err) {
-    fastify.log.error(err)
-    process.exit(1)
-  }
+function add (a, b) {
+  return a + b
 }
 
-start()
+module.exports = add
